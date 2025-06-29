4bb3ca17f99e02db86de9b4b71b1dad58789dab5
prithagupta05 <prithagupta004@gmail.com>
2025-06-29 18:32:15 +0530

    Delete portfolio directory

--- portfolio/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  
-    path('homepage/<int:id>', views.homePage),
-    path('aboutus', views.aboutUs),
-    path('studentform',views.student_form)
-]
