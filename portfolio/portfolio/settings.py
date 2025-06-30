1b3d8a5e2929460d715a3e30916c6620194e162e
Pritha <prithagupta>
2025-06-30 09:01:45 +0530

    images resolved locally

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-=8^howhs5n1-ha8pk2hx=7qg5tf_d9qy26sjxn7d6=yppb5l%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['prithagupta04.pythonanywhere.com']
+# ALLOWED_HOSTS = ['prithagupta04.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,6 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp', 'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
